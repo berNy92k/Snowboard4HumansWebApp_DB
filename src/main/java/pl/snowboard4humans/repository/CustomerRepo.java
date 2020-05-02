948970e87486c115b6e84ae0fb21fbc5c3ad64b4
Dawid Bernacki <daw.bernacki@gmail.com>
2020-05-03 00:19:18 +0200

    Main changes: add repository interface - add CustomerRepo.java

---
+++ src/main/java/pl/snowboard4humans/repository/CustomerRepo.java
@@ -0,0 +1,12 @@
+package pl.snowboard4humans.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.snowboard4humans.model.Customer;
+
+import java.util.List;
+
+public interface CustomerRepo extends JpaRepository<Customer, Integer> {
+
+    List<Customer> findCustomersByEmail(String email);
+
+}
